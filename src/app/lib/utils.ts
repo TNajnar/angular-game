30cdea6a097902197fd64fb0ed7527afb908b029
Tomas Najnar <tomas.najnar@regiojet.cz>
2024-09-06 13:39:41 +0200

    Unique id

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,5 +1,9 @@
 import { IRandomNumbers } from "./types";
 
+function generateId(): number {
+  return Math.floor(Math.random() * 1000);
+}
+
 function handleWinText(enemyHealth: number, heroHealth: number, heroName: string): string {
   return heroHealth > enemyHealth ? `${heroName} won` : 'Monster won';
 }
@@ -15,4 +19,4 @@ function randomNumbers(lenght: number): IRandomNumbers {
   return { number1: randomEquipNumber1, number2: randomEquipNumber2 };
 }
 
-export { handleWinText, randomNumber, randomNumbers }
+export { generateId, handleWinText, randomNumber, randomNumbers }
